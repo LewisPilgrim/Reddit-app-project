d6037b14735371c012ddb434f65bb6a586b7b223
Lewis Pilgrim <lewis.pilgrim0@hotmail.co.uk>
2022-08-31 09:16:45 +0100

    add tile display and search function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './Images/reddit-logo-svg.svg';
 import './App.css';
+import { useState } from 'react';
 
 import TileList from './features/TileList/TileList';
 
 
 function App() {
+
+  const [query, setQuery] = useState('');
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,12 +16,12 @@ function App() {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>RedditLite</h2>
         </div>
-        <input name='searchBar' type='text' placeholder='Search'></input>
-        <button className="login-button">Login</button>
+        <input name='searchBar' type='text' placeholder='Search' onChange={event => setQuery(event.target.value)}></input>
+        <button className="login-button" onClick={() => alert('Login function currently not supported.')} >Login</button>
       </header>
       <section>
         <h1>Popular posts</h1>
-        <TileList data-testid='tile' className='tileList' />
+        <TileList data-testid='tile' className='tileList' filter={query} />
       </section>
     </div>
   );
--- src/features/TileList/TileList.js
+++ src/features/TileList/TileList.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Tile from '../Tile/Tile';
+import '../../Images/potrait-flipped.jpg';
+import myData from '../../data.json';
 
-export default function TileList() {
+export default function TileList(props) {
+    const ids = [];
+    ids.push(myData);
+    const [posts, setPosts] = useState(ids[0]);
+    const query = props.filter;
+    console.log(posts[0].title.toLowerCase());
+    console.log(query);
+
+    const addPost = ({ id, title, text, comments }) => {
+        setPosts([...posts, {id, title, text, comments }])
+    }
+
     return (
-        <div title='box'>
-            <Tile className='tile' />
-            <Tile className='tile' />
-            <Tile className='tile' />
-            <Tile className='tile' />
+        <div>
+            {posts.filter((post) => query !== undefined ? post.title.toLowerCase().includes(query.toLowerCase()) : '').map((post) => {
+                return <Tile key={post.key} title={post.title} text={post.text} comments={post.comments} image={post.image} />
+            })}
         </div>
     )
 }
